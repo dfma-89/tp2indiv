478a1f6e6af1bdf71ddd39c08f8940cb5250044b
dfma-89 <dfma.mdp@gmail.com>
2022-07-18 15:15:03 -0300

    :sparkle: VARIABLES JS

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log ("Tarea");
+console.log ("Crear variable con tu nombre, una con tu edad, una si estas o no en pareja T/F, una con un array del 1 al 10, array con 5 nombres de skillers, un objeto que contenga el valor de todas las variables anteriores -handle-mode");
+
+const name = "Deborah Medina Alvarado";
+const age = "33";
+const marriedYN = true;
+const numerosArray = [1,2,3,4,5,6,7,8,9,10];
+const nombresSki = ['Abel Reyes', 'Alejandro Toloza', 'Bruno Rivera', 'Camila Victoria Stefano', 'Celeste DAngelo'];
+
+const myFirstObject = {
+    name: "Deborah Medina Alvarado",
+    age: "33",
+    marriedYN: true,
+    numerosArray: [1,2,3,4,5,6,7,8,9,10],
+    nombresSki: ['Abel Reyes', 'Alejandro Toloza', 'Bruno Rivera', 'Camila Victoria Stefano', 'Celeste DAngelo'],
+};
+
+console.log (myFirstObject);
+console.log (name);
+console.log (age);
+console.log (marriedYN);
+console.log (numerosArray);
+console.log (nombresSki);
